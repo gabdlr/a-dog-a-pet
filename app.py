d74faad9ec4a5c1bd92e2d724eda9c7f67cd00d2
Gabriel De Los Rios <gab.delosrios@gmail.com>
2024-09-22 13:58:36 -0300

    build: adds SQLAlchemy ORM

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template
 
-
+db = SQLAlchemy()
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///adogapet.db"
+db.init_app(app)
 
 @app.route("/")
 def hello_world():
