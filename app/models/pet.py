36faa5a71ee4b1573c3e4587f8f0ff1e13a34845
Gabriel De Los Rios <gab.delosrios@gmail.com>
2024-11-02 18:29:52 -0300

    fix: adds missing field

--- app/models/pet.py
+++ app/models/pet.py
@@ -11,5 +11,7 @@ class Pet(db.Model):
   location = mapped_column(db.String(255), nullable=False)
   sex = mapped_column(db.String(1), nullable=False)
   height = mapped_column(db.Float, default=0, nullable=False)
+  registrar_id = mapped_column(db.Integer, db.ForeignKey('user.id'), nullable=False)
   registration_date = mapped_column(db.DateTime, default=datetime.datetime.utcnow, nullable=False)
   kind = relationship("PetKind")
+  registrar = relationship("User")
