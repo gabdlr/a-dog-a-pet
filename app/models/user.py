2eb6de9df07e16e50cc0279453c7c9c4af37e2e8
Gabriel De Los Rios <gabriel.delosrios@tutamail.com>
2024-11-04 23:27:33 -0300

    fix: updates user model password max length

--- app/models/user.py
+++ app/models/user.py
@@ -6,7 +6,7 @@ class User(db.Model):
   id = mapped_column(db.Integer, primary_key=True)
   username = mapped_column(db.String(255), nullable=False)
   email = mapped_column(db.String(255), nullable=False)
-  password = mapped_column(db.Text, nullable=False)
+  password = mapped_column(db.String(255), nullable=False)
   name = mapped_column(db.String(255), nullable=False)
   lastname = mapped_column(db.String(255), nullable=False)
   birth_date = mapped_column(db.Date, nullable=False)
