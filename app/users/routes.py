1f3feece2f18b29af54d6e416e2a9fa011baa088
Gabriel De Los Rios <gabriel.delosrios@tutamail.com>
2024-11-05 00:26:12 -0300

    feat(user): adds registration form template

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app.users import bp
-
+from app.utils.form_errors_dict import FORM_ERRORS
 @bp.route('/')
 def index():
     return render_template("users/index.html")
@@ -11,4 +11,4 @@ def login():
 
 @bp.route('/register')
 def register():
-    return render_template("users/register.html")
+    return render_template("users/register.html", FORM_ERRORS=FORM_ERRORS)
