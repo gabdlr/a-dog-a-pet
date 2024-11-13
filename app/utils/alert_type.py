ee21a194ca094509096816385d975938839de920
Gabriel De Los Rios <gabriel.delosrios@tutamail.com>
2024-11-12 23:18:20 -0300

    feat: adds flash message

---
+++ app/utils/alert_type.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class AlertType(Enum):
+  PRIMARY="primary"
+  SECONDARY="secondary"
+  SUCCESS="success"
+  DANGER="danger"
+  WARNING="warning"
+  INFO="info"
+  LIGHT="light"
+  DARK="dark"
---
+++ app/utils/flash_message.py
@@ -0,0 +1,4 @@
+class FlashMessage:
+  def __init__(self, message, alertType):
+    self.message = message
+    self.alertType = alertType
