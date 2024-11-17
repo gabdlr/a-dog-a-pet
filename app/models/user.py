7541548acf5935ae28374c788211cf9f6634ddd8
Gabriel De Los Rios <gabriel.delosrios@tutamail.com>
2024-11-16 23:24:56 -0300

    fix: properly imports datetime, adds autoincrement

--- app/models/user.py
+++ app/models/user.py
@@ -1,9 +1,9 @@
-import datetime
+from datetime import datetime
 from sqlalchemy.orm import mapped_column
 from app.extensions import db
 
 class User(db.Model):
-  id = mapped_column(db.Integer, primary_key=True)
+  id = mapped_column(db.Integer, primary_key=True, autoincrement=True)
   username = mapped_column(db.String(255), nullable=False)
   email = mapped_column(db.String(255), nullable=False)
   password = mapped_column(db.String(255), nullable=False)
@@ -12,4 +12,4 @@ class User(db.Model):
   birth_date = mapped_column(db.Date, nullable=False)
   address = mapped_column(db.String(255), nullable=False)
   phone_number = mapped_column(db.String(255), nullable=False)
-  registration_date = mapped_column(db.DateTime, default=datetime.datetime.utcnow, nullable=False)
+  registration_date = mapped_column(db.DateTime, default=datetime.utcnow, nullable=False)
