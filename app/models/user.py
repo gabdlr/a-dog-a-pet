5e39131856763a6822750fcd07c9f564c531962b
Gabriel De Los Rios <gabriel.delosrios@tutamail.com>
2024-11-17 18:19:08 -0300

    fix: adds uniqueness constraint to users email field

--- app/models/user.py
+++ app/models/user.py
@@ -5,7 +5,7 @@ from app.extensions import db
 class User(db.Model):
   id = mapped_column(db.Integer, primary_key=True, autoincrement=True)
   username = mapped_column(db.String(255), nullable=False)
-  email = mapped_column(db.String(255), nullable=False)
+  email = mapped_column(db.String(255), nullable=False, unique=True)
   password = mapped_column(db.String(255), nullable=False)
   name = mapped_column(db.String(255), nullable=False)
   lastname = mapped_column(db.String(255), nullable=False)
