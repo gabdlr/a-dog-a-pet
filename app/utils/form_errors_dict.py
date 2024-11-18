f3f4912e41228352b5abc96e2c8f3dbfd3f57ecc
Gabriel De Los Rios <gabriel.delosrios@tutamail.com>
2024-11-17 22:39:04 -0300

    feat(users): adds in client password matching validation

--- app/utils/form_errors_dict.py
+++ app/utils/form_errors_dict.py
@@ -1,6 +1,7 @@
 FORM_ERRORS = {
   'REQUIRED': "This field is required.",
   'PASSWORD_LENGTH': 'It must be at least 6 characters long.',
+  'MUST_MATCH_PASSWORD': 'It must match the given password.',
   'VALID_EMAIL': 'Enter a valid email address.',
   'PHONE_NUMBER_FORMAT': 'It can be prefixed with the "+" sign and may only contain numbers after it.',
   'NAME_FORMAT': 'It must be at least 2 characters long, it may only contain letters.'
